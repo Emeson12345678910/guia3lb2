2bb97fc090a5677b6a403de077fdabb2faa30102
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2023-09-29 20:45:24 -0600

    Add files via upload

---
+++ GUIA 3 EJERCICIO 1/GUIA 3 EJERCICIO 1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Ejercicio1
+{
+    static void Main()
+    {
+
+        int[] years = new int[10] { 2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021 };
+
+        foreach (int year in years)
+        {
+            if (DateTime.IsLeapYear(year))
+            {
+                Console.WriteLine($"El año {year} ES BISIESTO.");
+            }
+            else
+            {
+                Console.WriteLine($"El año {year} NO ES BISIESTO. ");
+            }
+        }
+
+    }
+}
