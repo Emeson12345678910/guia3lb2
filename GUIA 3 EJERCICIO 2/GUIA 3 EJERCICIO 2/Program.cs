eeef6cf425347e74bf3ed6c3a06d6e9ec085d4d5
emerson <124007178+Emeson12345678910@users.noreply.github.com>
2023-09-29 20:48:10 -0600

    Add files via upload

---
+++ GUIA 3 EJERCICIO 2/GUIA 3 EJERCICIO 2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Globalization;
+
+class Program
+{
+    static void Main()
+    {
+        double prestamo = 3000; 
+        int plazo = 6;    
+        DateTime fechaInicio = DateTime.Now; 
+
+        double cuota_Mensual = prestamo / plazo;
+
+ 
+        Console.WriteLine($"Monto del préstamo: ${prestamo}");
+        Console.WriteLine($"Plazo en meses: {plazo} meses");
+        Console.WriteLine($"Cuota mensual: ${cuota_Mensual:F2}");
+        Console.WriteLine("Fechas de pago futuras:");
+
+        //FORMULA
+        for (int i = 1; i <= plazo; i++)
+        {
+            DateTime fechacancelar = fechaInicio.AddMonths(i);
+            Console.WriteLine($"Cuota {i}: {fechacancelar.ToString("dd-MM-yyyy")}, Cuota a pagar: ${cuota_Mensual: Pague tacaño}");
+        }
+    }
+}
---
+++ GUIA 3 EJERCICIO 3/GUIA 3 EJERCICIO 3/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+      
+        Console.Write("Por favor, ingresa tu fecha de cumpleaños  ");
+        string Usuario = Console.ReadLine();
+
+        if (DateTime.TryParse(Usuario, out DateTime fecha_Cumpleaños))
+        {
+         
+            DateTime fechaActual = DateTime.Now;
+
+           
+            DateTime dias_faltantes = new DateTime(fechaActual.Year, fecha_Cumpleaños.Month, fecha_Cumpleaños.Day);
+
+            if (dias_faltantes < fechaActual)
+            {
+                dias_faltantes = dias_faltantes.AddYears(1);
+            }
+
+            TimeSpan tiempoRestante = dias_faltantes - fechaActual;
+            int diasRestantes = tiempoRestante.Days;
+
+            Console.WriteLine($"Tu próximo cumpleaños es el {dias_faltantes.ToString("MM/dd/yyyy")}");
+            Console.WriteLine($"Faltan {diasRestantes}  para próximo cumpleaños.");
+        }
+        else
+        {
+            Console.WriteLine("fecha incorrecta Por favor, ingresa la fecha en el formato correcto (MM/DD/YYYY) EJEMPLO 27/07/2027.");
+        }
+    }
+}
